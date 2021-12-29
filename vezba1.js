a9d4bdf1e8520a71b0f826b6ca3062f750740939
Munevera <plojovicm3@gmail.com>
2021-12-29 23:13:24 +0100

    javascript

---
+++ vezba1.js
@@ -0,0 +1,42 @@
+// a = window.prompt("Unesi broj;");
+
+// if (a % 2 === 0) {
+//   console.log("broj je paran");
+// } else {
+//   console.log("broj je neparan");
+// }
+
+//god = 2003;
+//mes = 12;
+//dan = 13;
+//a = window.prompt("Unesite godinu:");
+//b = window.prompt("Unesite mesec:");
+//c = window.prompt("Unesite dan:");
+//console.log(2021 - god);
+//console.log(12 - mes);
+//console.log(30 - 13);//
+
+// a = window.prompt("Unesite broj");
+// if (a == 0) {
+//   console.log("Uneti broj je jednak nuli");
+// }
+// if (a % 2 == 0) {
+//   console.log("Uneti broj je paran");
+// }
+// if (a % 2 == 1) {
+//   console.log("Uneti broj je neparan");
+// }
+// //drugi zadatak/
+// a = window.prompt("Unesite prvi broj");
+// b = window.prompt("Unesite drugi broj");
+// c = window.prompt("Unesite treci broj");
+// let d = Math.max(a, b, c);
+// console.log(d);
+
+//treci zadatak/
+// a = window.prompt("Unesite prvi broj");
+// b = window.prompt("Unesite drugi broj");
+// c = window.prompt("Unesite treci broj");
+// var brojevi = [a, b, c];
+// brojevi.sort();
+// console.log(brojevi);
