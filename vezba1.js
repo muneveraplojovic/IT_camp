c1a402c20c1ad4f7902245c7ca96670829b979fa
Munevera <plojovicm3@gmail.com>
2022-02-15 18:15:31 +0100

    js

--- vezba1.js
+++ vezba1.js
@@ -1704,22 +1704,13 @@ var cat = {
   hunger: 0,
   lonliness: 0,
   happiness: 0,
-  play: function () {
-    this.hunger += 5;
-    this.hunger += 5;
-    this.happiness += 20;
-  },
+
   eat: function () {
     this.hunger -= 5;
     this.happiness += 5;
   },
-  pet: function () {
-    this.lonliness -= 5;
-    this.tiredness += 5;
-  },
+
   sleep: function () {
     this.tiredness -= 5;
   },
 };
-
-console.log(play());
