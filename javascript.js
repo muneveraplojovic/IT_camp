570ec5763ec24aeba57d5ebe7e4009ad6ff52f83
Munevera <plojovicm3@gmail.com>
2022-03-27 01:08:47 +0100

    js

--- javascript.js
+++ javascript.js
@@ -93,9 +93,24 @@
 //   }
 // }
 
-unos1 = Number(prompt("Unesite neki broj:"));
-unos2 = Number(prompt("Unesite jos jedan broj:"));
+// unos1 = Number(prompt("Unesite neki broj:"));
+// unos2 = Number(prompt("Unesite jos jedan broj:"));
 
-if (unos1 > unos2) {
-  unos1.push(broj);
-} else alert("Najveci uneti broj je broj", broj);
+// if (unos1 > unos2) {
+//   unos1.push(broj);
+// } else alert("Najveci uneti broj je broj", broj);
+
+
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item)
+  var removed=arr.shift()
+  return removed;
+  // Only change code above this line
+}
+
+// Setup
+const testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log(nextInLine(testArr, 6 ));
